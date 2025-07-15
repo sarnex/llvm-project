9933d139cb9663e574dc2aa9d45fe409c6143376
Sarnie, Nick <nick.sarnie@intel.com>
2025-07-15 07:32:45 -0700

    [LLVM][CMake] Pass -nostdinc++ to CMAKE_CXX_FLAGS when building with libc++
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>

--- llvm/cmake/modules/HandleLLVMStdlib.cmake
+++ llvm/cmake/modules/HandleLLVMStdlib.cmake
@@ -20,8 +20,9 @@ if(NOT DEFINED LLVM_STDLIB_HANDLED)
       check_cxx_compiler_flag("-stdlib=libc++" CXX_COMPILER_SUPPORTS_STDLIB)
       check_linker_flag(CXX "-stdlib=libc++" CXX_LINKER_SUPPORTS_STDLIB)
       if(CXX_COMPILER_SUPPORTS_STDLIB AND CXX_LINKER_SUPPORTS_STDLIB)
+	append("-nostdinc++" CMAKE_CXX_FLAGS)
         append("-stdlib=libc++"
-          CMAKE_CXX_FLAGS CMAKE_EXE_LINKER_FLAGS CMAKE_SHARED_LINKER_FLAGS
+           CMAKE_EXE_LINKER_FLAGS CMAKE_SHARED_LINKER_FLAGS
           CMAKE_MODULE_LINKER_FLAGS)
         set(LLVM_LIBCXX_USED 1)
       else()
